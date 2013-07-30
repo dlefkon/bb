086f8d6666a7bcb6fae96525bdd5a34a0f3ebf3c
dlefkon <dlefkon@gmail.com>
2013-07-29 22:00:07 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = 'mysql.allverticals.com';
-$db_user = 'dlefkon';
-$db_user_pw = 'bec45zak';
-$db_defdb = 'dlbb';
+$db_host = '';
+$db_user = '';
+$db_user_pw = '';
+$db_defdb = '';
 
 $conn = mysqli_connect($db_host,$db_user,$db_user_pw) or die ("Could not connect");
 mysqli_select_db($conn, $db_defdb) or die ("Could not select DB");
